54d93017cf4c345f201b76a71b84434730e55b49
Nataliy_Anohina2023 <117434665+Natalia2023@users.noreply.github.com>
2023-04-12 20:46:42 +0300

    result

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
+const CHANGE_SWITCH = 1000; //добавляю константу для зміни кольру
+let idInterval = null; //створюю змінну для зберігання початкового стану інтервалу
 
-const refs = {
-    changeBodyColor: document.querySelector('body'),
-    btnStart: document.querySelector('[data-start]'),
-    btnStop: document.querySelector('[data-stop'),
+// завантажується, зміна кольору фону ще не відбувається
+
+const btnStart = document.querySelector('button[data-start]');//дістаю кнопки з ДОМ дерева
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+btnStop.disabled = true;//Вимикаю кнопку стоп на початку, оскільки коли сторінка
+
+btnStart.addEventListener('click', onBtnStartChangeColor);//додаю слухачі подій на старт і стоп
+btnStop.addEventListener('click', onBtnStopChangeColor);
+
+function onBtnStartChangeColor() {
+  btnStart.disabled = true;//включаю старт 
+  btnStop.disabled = false;//виключаю стоп 
+
+  idInterval = setInterval(() => {//запускаю інтервал кожну сек
+    body.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_SWITCH);
 }
 
-let intervalId = null;
+function onBtnStopChangeColor() {
+  btnStart.disabled = false;//виключаю старт
+  btnStop.disabled = true;//включаю стоп
+
+  clearInterval(idInterval);//зупиняю інтервал
+}
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
-
-refs.btnStart.addEventListener('click', element => {
-    element.target.setAttribute('disabled', true);
-    refs.btnStop.removeAttribute('disabled');
-
-    intervalId = setInterval(() => {
-    refs.changeBodyColor.style.backgroundColor = getRandomHexColor();
-    }, 1000);
-});
-
-refs.btnStop.addEventListener('click', element => {
-    element.target.setAttribute('disabled', true);
-    refs.btnStart.removeAttribute('disabled');
-
-    clearInterval(intervalId);
-});
