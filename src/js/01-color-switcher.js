86b4191ef624170234d4ea83e0f33d09205db873
Nataliy_Anohina2023 <117434665+Natalia2023@users.noreply.github.com>
2023-04-13 13:31:59 +0300

    підкл бібліотеку

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const refs = {
+    changeBodyColor: document.querySelector('body'),
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop'),
+}
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+refs.btnStart.addEventListener('click', element => {
+   
+    element.target.setAttribute('disabled', true);
+    refs.btnStop.removeAttribute('disabled');
+
+    intervalId = setInterval(() => {
+    refs.changeBodyColor.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+refs.btnStop.addEventListener('click', element => {
+    element.target.setAttribute('disabled', true);
+    refs.btnStart.removeAttribute('disabled');
+
+    clearInterval(intervalId);
+});
+// Ця строчка JavaScript коду встановлює атрибут "disabled" для елементу, який був спрямований подією (event), що спричинила виклик функції,
+//  і встановлює його значення на "true", що робить елемент неактивним.
+
+// Це часто використовується для того, щоб 
+// заблокувати можливість взаємодії користувача з
+//  елементом на сторінці під час виконання якоїсь 
+//  дії, наприклад, при відправці форми або завантаженні даних.
